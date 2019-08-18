a88d985cf2c0e6e9fc926017dacd22469f706844
Yam Clement <yamtzunhei@gmail.com>
2019-08-18 11:51:50 +0200

    refactor Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css';
-import Header from './component/Header';
-import Weather from './component/Weather';
-import Footer from './component/Footer';
+import React from "react";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Weather />
-      <Footer />
-    </div>
-  );
+import "./App.css";
+import Header from "./component/Header";
+import Weather from "./component/Weather";
+import Footer from "./component/Footer";
+
+const latitude = "48.85341";
+const longitude = "2.3488";
+const apiKeyWeather = "9002d79af400e68e99903bb3b7fbd7ed";
+const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKeyWeather}/${latitude},${longitude}?units=si`;
+
+class App extends React.Component {
+  state = {
+    weather: []
+  };
+
+  componentDidMount() {
+    Axios.get(url)
+      .then(responseData => this.setState({ weather: responseData.data }))
+      .catch(error =>
+        console.log("Error from fecthing and parsing data", error)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Weather data={this.state.weather}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import Axios from 'axios';
-
 import CurrentWeather from './CurrentWeather';
-
-const latitude = '48.85341';
-const longitude = '2.3488';
-const apiKeyWeather = '9002d79af400e68e99903bb3b7fbd7ed';
-const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKeyWeather}/${latitude},${longitude}?units=si`;
 
 class Weather extends React.Component {
   
-  state = {
-    weather: []
-  }
-
-  componentDidMount(){
-    Axios.get(url)
-      .then(responseData => this.setState({weather: responseData.data}))
-      .catch(error => console.log("Error from fecthing and parsing data", error));
-  }
-
   render(){
     const {weather} = this.state;
     console.log(weather);
