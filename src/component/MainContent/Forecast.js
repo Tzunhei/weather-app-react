cfd3dfc41bd89962473597672bd9477d70550dc2
Yam Clement <yamtzunhei@gmail.com>
2019-08-25 11:49:56 +0200

    finish mvp

--- src/component/MainContent/Forecast.js
+++ src/component/MainContent/Forecast.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Weather from "./Weather";
 
 const Forecast = ({ data }) => {
   const dayOfWeekIndex = {
@@ -27,30 +26,50 @@ const Forecast = ({ data }) => {
     11: "December"
   };
 
-  let forecast;
-  if (!data) {
-    return null;
-  } else {
-    forecast = data.data.map(day => {
-      const forecastDate = new Date(day.time * 1000);
-      const dayOfWeek = dayOfWeekIndex[forecastDate.getDay()];
-      const dayNumber = forecastDate.getDate();
-      const month = monthIndex[forecastDate.getMonth()];
-      return (
-        <Weather
-        key={day.time}
-        temperature={day.temperatureHigh}
-        humidity={day.humidity}
-        precipProbability={day.precipProbability}
-        summary={day.summary}
-      >
-        <li>{`${dayOfWeek} ${dayNumber} ${month}`}</li>
-      </Weather>
-      );
-    });
+  const formatTemperature = (temperature) => {
+    return Math.floor(temperature);
   }
 
-  return <main>{forecast}</main>;
+  const formatToPercentage = (value) => {
+    return Math.trunc(value * 100);
+  }
+
+  if (!data) return null;
+
+  return (
+    <main>
+      <table className="table-container">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Low</th>
+            <th>High</th>
+            <th>Humidity</th>
+            <th>Precipitation</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.data.map(day => {
+            const forecastDate = new Date(day.time * 1000);
+            const dayOfWeek = dayOfWeekIndex[forecastDate.getDay()];
+            const dayNumber = forecastDate.getDate();
+            const month = monthIndex[forecastDate.getMonth()];
+            return (
+              <tr className="table-row">
+                <td>{`${dayOfWeek} ${dayNumber} ${month}`}</td>
+                <td>{formatTemperature(day.temperatureLow)} C°</td>
+                <td>{formatTemperature(day.temperatureHigh)} C°</td>
+                <td>{formatToPercentage(day.humidity)} %</td>
+                <td>{formatToPercentage(day.precipProbability)} %</td>
+                <td>{day.summary}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
 };
 
 export default Forecast;
--- src/component/MainContent/Hourly.js
+++ src/component/MainContent/Hourly.js
@@ -1,30 +1,49 @@
 import React from "react";
-import Weather from "./Weather";
 
 const Hourly = ({ data }) => {
+  
   if (!data) {
     return <p>Loading...</p>;
   }
 
   const next24HoursData = data.data.slice(0, 13);
 
+  const formatTemperature = (temperature) => {
+    return Math.floor(temperature);
+  }
+
+  const formatToPercentage = (value) => {
+    return Math.trunc(value * 100);
+  }
+
   return (
     <main>
-        {next24HoursData.map(hourlyData => {
-          const date = new Date(hourlyData.time * 1000);
-          const hour = date.getHours();
-          return (
-              <Weather
-                key={hourlyData.time}
-                temperature={hourlyData.temperature}
-                humidity={hourlyData.humidity}
-                precipProbability={hourlyData.precipProbability}
-                summary={hourlyData.summary}
-              >
-                <li>{`${hour}:00`}</li>
-              </Weather>
-          );
-        })}
+      <table className="table-container">
+        <thead>
+          <tr>
+            <th>Hour</th>
+            <th>Temperature</th>
+            <th>Humidity</th>
+            <th>Precipitation</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {next24HoursData.map(hourlyWeather => {
+            const date = new Date(hourlyWeather.time * 1000);
+            const hour = date.getHours();
+            return (
+              <tr key={data.time} className="table-row">
+                <td>{`${hour}:00`}</td>
+                <td>{formatTemperature(hourlyWeather.temperature)} C°</td>
+                <td>{formatToPercentage(hourlyWeather.humidity)} %</td>
+                <td>{formatToPercentage(hourlyWeather.precipProbability)} %</td>
+                <td>{hourlyWeather.summary}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 };
